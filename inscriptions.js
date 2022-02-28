49b0146239f6479f33d80afa92d7f396161c6383
eliasparada <eliasparadalozano@gmail.com>
2022-02-27 23:41:14 -0600

    Solucion de error al cerrar inscripciones

--- inscriptions.js
+++ inscriptions.js
@@ -150,7 +150,7 @@ Vue.component('inscriptions', {
                 <h1 class="text-lg md:text-2xl font-bold">Incribir Materias</h1>
             </div>
             <div class="flex items-center">    
-                <button class="bg-red-500 text-white w-fit p-2 rounded-lg" @click="close('subjects')">
+                <button class="bg-red-500 text-white w-fit p-2 rounded-lg" @click="close('inscriptions')">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
